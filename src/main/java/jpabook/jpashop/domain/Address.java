afb1e6658ef2568872d5ec1293773e316f1822c4
YoonSuHyeon <96-05-03@naver.com>
2022-02-14 21:24:04 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,11 +5,24 @@ import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
+/**
+ * 값 타입은 변경 불가능하게 설계해야함
+ * JPA 스펙상 엔티티나 임베디드 타입은 자바 기본 생성자 public,protected 설정 해야한다.
+ * JPA 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 지원해야하기 때문이다.
+ */
 @Embeddable
 @Getter
-@Setter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
