17576b75c18b0c0cad2022b684b65534ddbfa0d1
YoonSuHyeon <96-05-03@naver.com>
2022-02-15 22:43:14 +0900

    회원 repository,service 추가

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+
+    /**
+     * 기존에는 @PersistenceContext가 있어야 하지만 스프링 부트를 쓰는 경우
+     *
+     * @Autowired를 사용할 수 있기 때문에 이와같은 방벙을 사용 가능
+     */
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(em);
+    }
+
+    public Member findOne(Long memberId) {
+        return em.find(Member.class, memberId);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
